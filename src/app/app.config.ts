215478ed064b4f00e449ef2add8fccf04f0122d6
Jason Rider <jason.rider@primis.co.uk>
2025-04-23 14:51:31 -0700

    remove importProvidersFrom

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,14 +11,13 @@ import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
       provideFirebaseApp(() => initializeApp(environment.firebase)),
       provideFirestore(() => getFirestore()),
       provideAuth(() => getAuth()),
       provideFunctions(() => getFunctions()),
       provideStorage(() => getStorage()),
       provideMessaging(() => getMessaging())
-    ),
+    ,
     provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"capstone-2025-ec7b8","appId":"1:169849532805:web:ac9709faee6022ea6854ec","storageBucket":"capstone-2025-ec7b8.firebasestorage.app","apiKey":"AIzaSyDTZ23iggM7dTehCQ93cxX3g3Mg_9uy9FI","authDomain":"capstone-2025-ec7b8.firebaseapp.com","messagingSenderId":"169849532805","measurementId":"G-28ZV8FGBB3"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
   ],
 };
