dc27aa248743ea796fd494ea3712018742c94dd5
Jason Rider <jason.rider@primis.co.uk>
2025-04-23 16:27:19 -0700

    Adding Google Authentication v2

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,10 +1,10 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDTZ23iggM7dTehCQ93cxX3g3Mg_9uy9FI",
-  authDomain: "capstone-2025-ec7b8.firebaseapp.com",
-  projectId: "capstone-2025-ec7b8",
-  storageBucket: "capstone-2025-ec7b8.firebasestorage.app",
-  messagingSenderId: "169849532805",
-  appId: "1:169849532805:web:ac9709faee6022ea6854ec",
-  measurementId: "G-28ZV8FGBB3"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDTZ23iggM7dTehCQ93cxX3g3Mg_9uy9FI",
+//   authDomain: "capstone-2025-ec7b8.firebaseapp.com",
+//   projectId: "capstone-2025-ec7b8",
+//   storageBucket: "capstone-2025-ec7b8.firebasestorage.app",
+//   messagingSenderId: "169849532805",
+//   appId: "1:169849532805:web:ac9709faee6022ea6854ec",
+//   measurementId: "G-28ZV8FGBB3"
+// };
